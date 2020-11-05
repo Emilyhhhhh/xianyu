f0abbcb9293fb386885ffc9271d75ac45331d0f9
xiaobai <123456>
2020-11-05 17:16:17 +0800

    引入页头，页脚，添加公共样式

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,32 @@
 <template>
   <div>
+    <!-- 这里相当于之前的app.vue -->
+    <!-- <router-view></router-view> -->
+    <UserHeader />
+    <!--  -->
     <Nuxt />
+    <UserFooter />
+
   </div>
 </template>
 
-<style>
+
+<script>
+import UserFooter from '@/components/user/UserFooter'
+import UserHeader from '@/components/user/UserHeader'
+
+export default {
+  components:{
+    UserHeader,UserFooter
+  }
+
+}
+</script>
+
+<style lang="less">
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -24,39 +36,19 @@ html {
   box-sizing: border-box;
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+*{
+  margin:0; 
+  padding:0;
+}
+ul, li, ol{
+  list-style:none;
+}
+a{
+  text-decoration:none;
+  color:inherit;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+em,i{
+  font-style: normal;
 }
 </style>
