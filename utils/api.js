3af8271a97c00132dbcfa45a41bcc315c0249550
xiaobai <123456>
2020-11-08 15:06:38 +0800

    删除了不必要的代码

--- utils/api.js
+++ utils/api.js
@@ -7,9 +7,9 @@
 // 远程服务器
 // 创建一个 axios
 import axios from 'axios'
-axios.defaults.baseURL
+
 const http = axios.create({
     baseURL: 'http://157.122.54.189:9095'
 })
-http.interceptors.response
+
 export default http
