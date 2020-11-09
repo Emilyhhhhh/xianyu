50baa51636a32b020e501d5e397b296ec0186a27
xiaobai <123456>
2020-11-09 13:14:27 +0800

    自己另外封装的 axios 使用的拦截器

--- utils/api.js
+++ utils/api.js
@@ -7,9 +7,29 @@
 // 远程服务器
 // 创建一个 axios
 import axios from 'axios'
-
 const http = axios.create({
     baseURL: 'http://157.122.54.189:9095'
 })
+
+http.interceptors.response.use(
+    res => {
+        console.log('这里拦截到正常响应状态码为200的请求');
+        // 如果是状态码就报错的请求, 这里无法拦截, 需要添加第二个函数作为回调
+        // 如果拦截了请求或者响应, 记得要 return
+        return res
+    },
+    err => {
+        console.log('这里拦截到状态码本身出错');
+        // 这里可以拦截到状态码本身出错的请求
+        console.dir(err)
+        Message.error(err.response.data.message || '系统错误')
+    }
+)
+
+
+
+
+
+
 
 export default http
