54c5de86e75c14039219fd3ac9f12a0c9f7abe2f
xiaobai <123456>
2020-11-09 21:01:19 +0800

    机票列表页-航班头部布局组件引入，航班列表组件引入，子组件样式结构已引入

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,15 +1,56 @@
 <template>
-  <div>
-      feiji
-  </div>
+    <section class="contianer">
+        <el-row  type="flex" justify="space-between">
+
+            <!-- 顶部过滤列表 -->
+            <div class="flights-content">
+                <!-- 过滤条件 -->
+                <div>
+                    
+                </div>
+                
+                <!--🚩🚩1. 航班头部布局 -->
+                <flightsListHead/>
+                
+                
+                <!-- 航班信息 -->
+                <div>
+                     <!--🚩🚩2. 航班列表 -->
+                    <flightsItem/>
+                </div>
+            </div>
+
+            <!-- 侧边栏 -->
+            <div class="aside">
+                <!-- 侧边栏组件 -->
+            </div>
+        </el-row>
+    </section>
 </template>
 
 <script>
+import flightsListHead from '@/components/air/flightsListHead.vue'
+import flightsItem from '@/components/air/flightsItem.vue'
 export default {
+    components: {
+        flightsListHead,flightsItem
+    }
 
 }
 </script>
 
-<style>
+<style scoped lang="less">
+    .contianer{
+        width:1000px;
+        margin:20px auto;
+    }
 
+    .flights-content{
+        width:745px;
+        font-size:14px;
+    }
+
+    .aside{
+        width:240px;
+    } 
 </style>
