5fea9424441e01a6fbd3998e54a38ee62460dc6a
xiaobai <123456>
2020-11-14 18:39:01 +0800

    订单表单的数据校验

--- components/air/orderForm.vue
+++ components/air/orderForm.vue
@@ -2,10 +2,23 @@
     <div class="main">
         <div class="air-column">
             <h2>乘机人</h2>
-            <el-form class="member-info">
+            <!-- medel  表单数据对象 -->
+            <el-form class="member-info" :model="{users}" ref="usersForm" style="border-bottom:15px">
                 <div class="member-info-item" v-for="(value,index) in users" :key='index'>
 
-                    <el-form-item label="乘机人姓名">
+                    <!--需求：从users里找到每一个username 绑定rules 
+                        遍历users得到每一条value 
+                        prop是定义需要规则的键 也就是users.username去找到值
+                        从users里找到每一个username   users.value.username
+                        value=users[index]
+                    -->
+
+                    <el-form-item label="乘机人姓名" :prop="`users[${index}].username`" :rules="[{
+                    required: true,        
+                    message: '请输入乘机人姓名', 
+                    trigger: 'blur' 
+                }]" >
+                      
                         <el-input v-model="value.username" placeholder="请输入姓名" class="input-with-select">
                             <el-select 
                             slot="prepend" 
@@ -16,7 +29,11 @@
                         </el-input>
                     </el-form-item>
 
-                    <el-form-item label="证件类型">
+                    <el-form-item label="证件类型" :prop="`users[${index}].id`" :rules="[{
+                    required: true, 
+                    message: '请输入证件号码', 
+                    trigger: 'blur' 
+                }]" >
                         <el-input 
                          v-model="value.id"
                         placeholder="请输入证件号码"  class="input-with-select">
@@ -35,7 +52,7 @@
 
             <el-button class="add-member" type='primary' @click="handleAddUsers">添加乘机人</el-button>
         </div>
-            
+          <!-- ---------------------保险-------------------------------   -->
         <div class="air-column">
             <h2>保险</h2>
             <div>
@@ -50,25 +67,25 @@
                 </el-checkbox-group>
             </div>
         </div>
-
+          <!-- ------------------------联系人----------------------------   -->
         <div class="air-column">
             <h2>联系人</h2>
             <div class="contact">
-                <el-form label-width="60px">
-                    <el-form-item label="姓名">
-                        <el-input v-model="contactName"></el-input>
+                <el-form label-width="80px" :rules="rules" ref="columnForm" :model="{contactName,contactPhone,captcha}">
+                    <el-form-item label="姓名" prop="contactName">
+                        <el-input v-model="contactName" @focus="clearRules('contactName')"></el-input>
                     </el-form-item>
 
-                    <el-form-item label="手机">
-                        <el-input placeholder="请输入内容"  v-model="contactPhone">
+                    <el-form-item label="手机" prop="contactPhone">
+                        <el-input placeholder="请输入内容" @focus="clearRules('contactPhone')" v-model="contactPhone">
                             <template slot="append">
                             <el-button @click="handleSendCaptcha">发送验证码</el-button>
                             </template>
                         </el-input>
                     </el-form-item>
 
-                    <el-form-item label="验证码">
-                        <el-input v-model="captcha"></el-input>
+                    <el-form-item label="验证码" prop="captcha">
+                        <el-input v-model="captcha" @focus="clearRules('captcha')"></el-input>
                     </el-form-item>
                 <el-checkbox  v-model="invoice">是否需要发票
                     </el-checkbox> 
@@ -82,6 +99,7 @@
 
 <script>
 import {captchas} from '@/myapi/user.js'
+
 export default {
     props: ['data'],
     data () {
@@ -92,20 +110,42 @@ export default {
                 // 里面的每个对象都是一个乘机人
                 // 添加删除的实话, 只需要 push / splice
                 {
-                    username:'xiaobai',
-                    id:'441611111111111111'
+                    username:'',
+                    id:''
                 }
             ],
             insurances:[],     //保险id
-            contactName:'xiaobai',     //联系人名字
-            contactPhone:'13722222222',    //电话
+            contactName:'',     //联系人名字
+            contactPhone:'',    //电话
             invoice:false,      //是否需要发票
-            captcha:'000000',
+            captcha:'',
+            rules:{
+                contactName:[{
+                    required: true, 
+                    message: '请输入联系人名字', 
+                    trigger: 'blur' 
+                }],
+
+                 contactPhone:[{
+                    required: true, 
+                    message: '请输入联系人电话', 
+                    trigger: 'blur' 
+                }],
+                 captcha:[{
+                    required: true, 
+                    message: '请输入验证码', 
+                    trigger: 'blur' 
+                }],
+            }
 
 
         }
     },
     methods: {
+         // 1.聚焦时清除规则
+        clearRules(pp){
+            this.$refs.columnForm.clearValidate(pp)
+        },
         // 移除乘机人
         handleDeleteUser(index){
             // console.log(index);
@@ -129,6 +169,11 @@ export default {
         },
         // 发送验证码
         async handleSendCaptcha(){
+            // 正则规则语法：规则.test(需要验证的字符)
+          let regexp= /^1[3456789]\d{9}$/
+          if(!regexp.test(this.contactPhone)){
+            return this.$message.error('请输入正确的手机号')
+          }
             let res = await captchas(this.contactPhone)
             if(res.status==200){
                 this.$message.success('验证码发送成功')
@@ -137,44 +182,68 @@ export default {
         },
         // 提交订单
          handleSubmit(){
-             let data= {
-                 users:this.users,
-                insurances:this.insurances,    
-                contactName:this.contactName,   
-                contactPhone:this.contactPhone,    
-                invoice:this.invoice,     
-                captcha:this.captcha,
-                seat_xid:this.$route.query.seat_xid,
-                air:this.$route.query.id
+            //  this.$refs.usersForm.validate((isValid) => {
+            //       console.log(isValid);
+            //         if (isValid) {
+            //             this.$refs.columnForm.validate((isValid) => {
+            //                 if (isValid) {
+            //                 }
+            //             })
+            //         }
+            // })
+         Promise.all([
+             this.$refs.usersForm.validate(),
+             this.$refs.columnForm.validate()
+         ]).then(res=>{
+              // 因为 Promise.all 传入了两个元素组成的数组
+              // 所以 res 也是一个数组对应,上面promise 的结果
+             if(res[0]&&res[1]){
+                   console.log("两个表单都交验完了, 应该发请求");
+                                  let data= {
+                                    users:this.users,
+                                    insurances:this.insurances,    
+                                    contactName:this.contactName,   
+                                    contactPhone:this.contactPhone,    
+                                    invoice:this.invoice,     
+                                    captcha:this.captcha,
+                                    seat_xid:this.$route.query.seat_xid,
+                                    air:this.$route.query.id
+                                  }
+                                //  console.log(this.data);
+                                //  console.log(data);
+
+                                //提交完了需要等一段时间
+                                this.$message.success({
+                                    message:'正在生成订单！请稍等',
+                                    duration:1000})
+
+                                //  发送请求
+                                this.$axios({
+                                    url:'/airorders',
+                                    method:'post',
+                                    //  Bearer是JWT定义的规范
+                                    headers:{
+                                        Authorization:'Bearer '+this.$store.state.userstore.userInfo.token
+                                    },
+                                    data
+                                }).then(res=>{
+                                    console.log(res);
+                                    this.$message.success(res.data.message)
+                                    let id=res.data.data.id
+                                    //跳转到付款页
+                                    this.$router.push({
+                                        path:'/air/pay',
+                                        query:{id}
+                                    })
+                                })
+
              }
-            //  console.log(this.data);
-            //  console.log(data);
+         })
 
-             //提交完了需要等一段时间
-             this.$message.success({
-                 message:'正在生成订单！请稍等',
-                 duration:1000})
+        
 
-            //  发送请求
-             this.$axios({
-                 url:'/airorders',
-                 method:'post',
-                //  Bearer是JWT定义的规范
-                 headers:{
-                     Authorization:'Bearer '+this.$store.state.userstore.userInfo.token
-                 },
-                 data
-             }).then(res=>{
-                 console.log(res);
-                 this.$message.success(res.data.message)
-                 let id=res.data.data.id
-                 //跳转到付款页
-                 this.$router.push({
-                     path:'/air/pay',
-                     query:{id}})
-
-             })
-        },
+         }
+            
     },
 
     computed:{
@@ -231,9 +300,11 @@ export default {
     .input-with-select /deep/  .el-input-group__prepend {
         background-color: #fff;
     }
-    .member-info /deep/ .el-form-item{
-        margin-bottom:0;
-    }
+
+    // 显示错误时，出现文字的下边距
+    // .member-info /deep/ .el-form-item{
+    //     margin-bottom:0;
+    // }
 
     .member-info-item{
         border-bottom:1px #eee dashed;
